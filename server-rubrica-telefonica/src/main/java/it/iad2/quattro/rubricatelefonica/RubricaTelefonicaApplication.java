3fe91d21a8d0f99c06db770180bfae4353370339
francescofilitti <79720164+francescofilitti@users.noreply.github.com>
2021-03-09 12:15:02 +0100

    commit 2
    ticket #2

---
+++ server-rubrica-telefonica/src/main/java/it/iad2/quattro/rubricatelefonica/RubricaTelefonicaApplication.java
@@ -0,0 +1,13 @@
+package it.iad2.quattro.rubricatelefonica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricaTelefonicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubricaTelefonicaApplication.class, args);
+	}
+
+}
---
+++ server-rubrica-telefonica/src/test/java/it/iad2/quattro/rubricatelefonica/RubricaTelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad2.quattro.rubricatelefonica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubricaTelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
